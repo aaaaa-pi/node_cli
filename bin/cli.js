bba41038eec79ecfa39f64b17b38d757b330a035
Eureka <euraka77035@gmail.com>
2022-11-17 16:15:15 +0800

    first commit

---
+++ bin/cli.js
@@ -0,0 +1,10 @@
+#! /usr/bin/env node
+const { program } = require('commander')
+const myhelp = require('../lib/core/help')
+const mycommander = require('../lib/core/mycommander')
+
+myhelp(program)
+
+mycommander(program)
+
+program.parse(process.argv)
---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    framwork: ['express', 'koa', 'egg'],
+    foramworkUrl: {
+        express: 'https://gitee.com/beiyaoyaoyao/express-template.git',
+        koa: 'https://gitee.com/beiyaoyaoyao/koa-template.git',
+        egg: 'https://gitee.com/beiyaoyaoyao/egg-template.git'
+    }
+}
